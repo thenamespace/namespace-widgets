4f9baedad27d57aa2563cb2610b9b841a9e29628
nenadmitt <nenadmitt@gmail.com>
2023-11-28 21:19:38 +0100

    add workflow

--- vanilla-js-mint-subnames/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './src/main.js', // Specify the entry point of your application
-  output: {
-    filename: 'bundle.js', // Specify the output file name
-    path: path.resolve(__dirname, 'dist'), // Specify the output directory
-  },
-};
